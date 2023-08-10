361c219e8d5be576d12d701f705c324b73ca8653
Binnur KURT <deepcloudlabs@gmail.com>
2023-08-10 14:41:21 +0300

    initial commit

--- lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
+++ lottery-microservice/src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
@@ -2,7 +2,7 @@ package com.example.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Spring Cloud Bus / Kafka
 @SpringBootApplication
 public class LotteryMicroserviceApplication {
 
