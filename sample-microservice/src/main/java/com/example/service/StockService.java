faf456e26f0fbe2ebc33f2629da48bd658b2dd8d
Binnur KURT <deepcloudlabs@gmail.com>
2023-08-11 15:56:39 +0300

    initial commit

--- sample-microservice/src/main/java/com/example/service/StockService.java
+++ sample-microservice/src/main/java/com/example/service/StockService.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 
 import com.example.domain.Product;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+
 @Service
 public class StockService {
 
+	@Bulkhead(name = "sku",type = Type.SEMAPHORE)
 	public Product findProduct(String sku) throws InterruptedException {
 		System.err.println("New request has arrived at %s".formatted(LocalDateTime.now()));
 		TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(6));
